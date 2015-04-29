51b9e63f1c6345615f31a0bb7d4905de5faf6f01
Abe Hiroshi <abe@x-wave.co.jp>
2015-04-30 00:37:35 +0900

    initial

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+alias tutum='docker run -e TUTUM_USER=$TUTUM_USER -e TUTUM_APIKEY=$TUTUM_APIKEY -v $(pwd):/app abehiroshi/tutum-cli'
+
+STACKNAME=${TUTUM_STACKNAME-worklog}:${IMAGE_TAG-latest}
+STACKFILE=tutum-${IMAGE_TAG-latest}.yml
+
+echo "tutum stack up ${STACKNAME}"
+tutum stack update -f "/app/${STACKFILE}" "${STACKNAME}" || tutum stack up -n "${STACKNAME}" -f "/app/${STACKFILE}"
